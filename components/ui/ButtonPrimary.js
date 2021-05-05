8c79d0c0612fe6c4cb569bd3194dd63882e09a10
Josef Bauer <josef.bauer.1st@gmail.com>
2021-05-05 13:16:03 +0200

    Fix ButtonPrimary in wrong file

--- components/ui/ButtonPrimary.js
+++ components/ui/ButtonPrimary.js
@@ -1,7 +1,7 @@
-export default function ButtonPrimary({props}) {
+export default function ButtonPrimary({children}) {
   return (
-    <div className="rounded bg-green-300 w-max py-1 px-3 hover:bg-green-400 cursor-pointer">
-      {props.text}
+    <div className="rounded bg-green-300 w-max py-1 px-3 hover:bg-green-400 cursor-pointer text-white">
+      {children}
     </div>
   )
 }
