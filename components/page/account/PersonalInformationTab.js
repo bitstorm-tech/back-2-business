a1cc661d5f293d2676efd3bc3b11bf89d31812f5
Josef Bauer <josef.bauer.1st@gmail.com>
2021-05-05 12:28:36 +0200

    Add tab bar with two tab content components to account page

---
+++ components/page/account/PersonalInformationTab.js
@@ -0,0 +1,5 @@
+export default function PersonalInformationTab() {
+  return (
+    <h1>Personal Information</h1>
+  );
+}
---
+++ components/ui/TabBar.js
@@ -0,0 +1,26 @@
+import { useState } from 'react';
+
+export default function TabBar({children}) {
+  const [activeTabId, setActiveTabId] = useState(0);
+
+  function activeTab(id) {
+    return id === activeTabId ? ' text-blue-500 border-b-2 font-medium border-blue-500' : '';
+  }
+
+  return (
+    <>
+      <div className="bg-white">
+        <nav className="flex flex-col sm:flex-row justify-center">
+          {children && children.map((child, i) =>
+            <button key={i} onClick={() => setActiveTabId(i)}
+                    className={'text-gray-600 py-4 px-6 block hover:text-blue-500 focus:outline-none' + activeTab(i)}>
+              {child.props.tabText}
+            </button>)}
+        </nav>
+      </div>
+      <div>
+        {children && children[activeTabId]}
+      </div>
+    </>
+  );
+}
