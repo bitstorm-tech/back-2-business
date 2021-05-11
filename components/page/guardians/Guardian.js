eacccad8e69e57ab2a890e137218dbd514646dbe
Josef Bauer <josef.bauer.1st@gmail.com>
2021-05-11 11:32:07 +0200

    Improved Time Guardians page

--- components/page/guardians/Guardian.js
+++ components/page/guardians/Guardian.js
@@ -2,16 +2,18 @@ import PrimaryButton from '../../ui/buttons/PrimaryButton';
 import { useState } from 'react';
 import DeleteButton from '../../ui/buttons/DeleteButton';
 
-export default function Guardian({name, onDelete, key, editable = false}) {
+export default function Guardian({name, onDelete, onSave, editable = false}) {
   const [isEditable] = useState(editable);
   return (
     <div className="bg-white p-6 rounded-xl shadow-xl">
-      <p className="text-xl font-semibold mb-2">{name}</p>
+      <div>
+        <p className="text-xl font-semibold mb-2">{name}</p>
+      </div>
       <div className="mb-2">Content</div>
-      <div className="pt-4 w-full flex flex-row-reverse">
-        <DeleteButton onDelete={() => onDelete(key)} />
-        <PrimaryButton>Save</PrimaryButton>
+      <div className="pt-4 flex flex-row space-x-2">
+        <PrimaryButton onSave={onSave}>Save</PrimaryButton>
+        <DeleteButton onDelete={onDelete}/>
       </div>
     </div>
-  )
+  );
 }
--- pages/guardians.js
+++ pages/guardians.js
@@ -6,29 +6,37 @@ import Guardian from '../components/page/guardians/Guardian';
 import { useState } from 'react';
 
 function Guardians() {
+  let counter = 1;
   const {t} = useTranslation('guardians');
-  const [guardians, setGuardians] = useState([])
+  const [guardians, setGuardians] = useState([]);
 
-  function addGuardian() {
-    setGuardians([...guardians, '']);
+  function createNewGuardian() {
+    setGuardians([...guardians, {name: `New Guardian #${counter++}`}]);
   }
 
-  function deleteGuardian(index) {
-    guardians.splice(index, 1);
-    setGuardians([...guardians]);
+  function removeGuardian(name) {
+    setGuardians([...guardians.filter(guardian => guardian.name !== name)]);
   }
 
   return (
     <div className="m-5">
-      <div className="flex flex-row justify-center space-x-4">
-        <PrimaryButton onClick={addGuardian}>{t('create-new-guardian')}</PrimaryButton>
-        <PrimaryButton>{t('invite-existing-guardian')}</PrimaryButton>
+      <div className="flex flex-row justify-center space-x-4 mb-5">
+        <PrimaryButton onClick={() => createNewGuardian()}>
+          {t('create-new-guardian')}
+        </PrimaryButton>
+        <PrimaryButton>
+          {t('invite-existing-guardian')}
+        </PrimaryButton>
       </div>
-      <div className="flex flex-row space-x-4 justify-center flex-wrap mt-2">
-        {guardians.map((guardian, i) => <Guardian key={i} name={i} onDelete={deleteGuardian}/>)}
+      <div className="flex flex-row space-x-4 justify-center flex-wrap">
+        {guardians.map((guardian, i) =>
+          <div key={i} className="m-2">
+            <Guardian name={guardian.name} onDelete={() => removeGuardian(guardian.name)}/>
+          </div>
+        )}
       </div>
     </div>
-  )
+  );
 }
 
 export default withPageAuthRequired(Guardians);
