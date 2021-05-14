efdf57b5a3d08e7acb2ebbbfddf12f544a8cb6fb
Josef Bauer <josef.bauer.1st@gmail.com>
2021-05-14 12:36:06 +0200

    Some minor design tweaks

--- components/ui/TabBar.js
+++ components/ui/TabBar.js
@@ -4,7 +4,7 @@ export default function TabBar({children}) {
   const [activeTabId, setActiveTabId] = useState(0);
 
   function activeTab(id) {
-    return id === activeTabId ? ' text-red-500 border-b-4 font-medium border-red-500' : '';
+    return id === activeTabId ? ' text-red-500 border-b-4 font-medium border-red-500' : ' text-indigo-200';
   }
 
   return (
@@ -13,7 +13,7 @@ export default function TabBar({children}) {
         <nav className="flex flex-col sm:flex-row justify-center">
           {children && children.map((child, i) =>
             <button key={i} onClick={() => setActiveTabId(i)}
-                    className={'text-indigo-200 py-4 px-6 block hover:text-red-500 focus:outline-none' + activeTab(i)}>
+                    className={'py-4 px-6 block hover:text-red-500 focus:outline-none' + activeTab(i)}>
               {child.props.tabText}
             </button>)}
         </nav>
